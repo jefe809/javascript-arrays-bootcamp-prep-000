4390d55d21d485f804b00d48b63a5fabd24d7a6b
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-03 23:27:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice()
+  array = array.slice()
   return array
 }
 
