ec388c51ce9d1fd85cea7adeb1c26d311aec4061
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-03 21:52:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray([], string){
-  return ["", ...[]]
-  return []
+  return [string, ...[]]
+  
 }
 
 function destructivelyAddElementToBeginningOfArray(){
