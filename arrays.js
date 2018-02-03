ada55790b7d55f6c8d8cab6b38366e37f1512d64
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-03 23:08:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  return [1]
+  return [element]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
