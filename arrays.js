e3627c29875aafa4e381876dbb9e40fa5279dfcb
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-03 22:22:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,5 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-   return new Array([element, array])
+   return new Array([array, element])
 }
